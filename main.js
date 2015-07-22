c8a28bf778c358e662d10fc776887f3a34fe0a8d
raviteja2200 <raviteja2200@gmail.com>
2015-07-21 19:08:41 -0700

    added work regarding javascript

---
+++ main.js
@@ -0,0 +1,61 @@
+for (var i = 0; i <= 100; i++) {
+	console.log("Hello world "+i);
+};
+
+
+/*function go () {
+	// body...
+	alert('hi');
+}
+//arrays
+
+var myList = ['apples','orange','Bananas'];
+myList[3] = 'pineapples';
+myList[0] = 'watermelon';
+
+//we can use foreach which loop through each element in array but it is new it is not supported by every browser
+//loops 
+
+var times = 0;
+while (times<10){
+	console.log('tried it', times);
+	times++;
+}
+
+// for is the most used argument in JS
+
+// it takes three arguments
+
+//it is same as in all computer languages
+
+//for(setup,comparision,change)
+//for (var i=0;i<10;i++){}
+for (var i=0;i<myList.length; i++){
+	alert('you have '+ myList[i]+ ' in your bag');
+}
+*/
+//java script events
+/*-click- wehn browser registers click
+-mouseenter- 
+-mouseleave
+-mousedown
+-mouseup
+-mousemove
+-keydown-these are key events
+-keyup
+-blur
+-focus
+there are so many events find them on internet
+*//*
+number string boolean object array
+you declare a variable in javascript using var key word
+var keyword is something where u can hold any value
+dont use var key word to use it as a global variable
+it is only
+
+scope of varaible in JS
+java script concept called hoisting
+when u declare a variable outside of loop
+Statements are nothing but instructions which are used in programming
+
+*/
